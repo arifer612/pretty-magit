58a7572309a26095f43628db2cd7b90e70e2e5af
Arif Er <arifer612@pm.me>
2022-06-15 00:02:09 +0800

    refactor: include license header and remove unused cl-lib

--- pretty-magit.el
+++ pretty-magit.el
@@ -3,9 +3,25 @@
 
 ;; Copyright (C) 2022 Arif Er <arifer612@proton.me>
 
+;; This file is not part of GNU Emacs.
+;;
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 ;; Author: Arif Er <arifer612@proton.me>
 ;; Created: 29 May 2022
-;; Package-Requires: (cl-lib dash)
+;; URL: https://github.com/arifer612/pretty-magit
+;; Package-Requires: (dash)
 ;; Keywords: faces, vc
 
 ;;; Commentary:
@@ -25,7 +41,6 @@
 
 ;;; Code:
 
-(require 'cl-lib)
 (require 'dash)
 
 
